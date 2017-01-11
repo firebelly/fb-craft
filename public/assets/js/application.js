4a64bc829d49221c1704f8335fd06bd9855d4787
Nate Beaty <nate@firebellydesign.com>
2017-01-11 13:48:39 -0600

    update Thank You wording from Kristin after making an order

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -358,7 +358,7 @@ $.gdgr.main = (function() {
           setTimeout(function() { $('.cart').removeClass('working'); }, 2500);
           if (response.success) {
             _resetCart();
-            $('.cart-feedback').html('<h3>Thank you!</h3><p>Your order has been placed. You should receive an email receipt soon.</p>').slideDown('fast');
+            $('.cart-feedback').html('<h3>Thank you!</h3><p>Your order is confirmed and an email receipt is on the way.</p>').slideDown('fast');
           } else {
             $('.cart-feedback').html('<h3>Oh no!</h3><p>There was a transaction error: ' + response.error + '</p>').slideDown('fast');
           }
