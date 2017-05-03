8f9da5aec7a2fe6e4880148e94369c932d3a05f4
Nate Beaty <nate@clixel.com>
2017-05-03 10:58:27 -0500

    add requirements.txt for virtualenv (mkvirtualenv fb-craft && pip install -r requirements.txt), update Dotenv syntax

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,8 @@
 from fabric.api import *
 import os
-from os.path import join,dirname
-from dotenv import load_dotenv
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+from dotenv import Dotenv
+dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+os.environ.update(dotenv)
 
 env.hosts = ['craft.firebelly.co']
 env.user = 'firebelly'
