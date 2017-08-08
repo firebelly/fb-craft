ac3493623591fb6836eb890e3684f997f43609e7
Nate Beaty <nate@firebellydesign.com>
2017-08-08 16:15:46 -0500

    add clear_cache() method in deploy to clear template caches

--- fabfile.py
+++ fabfile.py
@@ -11,11 +11,13 @@ env.path = '/Users/nate/Sites/fb-craft'
 env.remotepath = '/home/firebelly/webapps/fb_craft'
 env.git_branch = 'master'
 env.warn_only = True
+env.remote_protocol = 'http'
 
 def production():
 	env.hosts = ['www.firebellydesign.com']
 	env.user = 'deployer'
 	env.remotepath = '/var/www/fb-craft/'
+	env.remote_protocol = 'https'
 	# env.user = 'firebelly'
 	# env.remotepath = '/home/firebelly/webapps/fb_craft'
 
@@ -25,20 +27,19 @@ def assets():
 def deploy():
 	update()
 	composer_install()
-	# clear_cache()
+	clear_cache()
 
 def update():
 	with cd(env.remotepath):
-		run('git pull origin %s' % env.git_branch)
+		run('git pull origin {0}'.format(env.git_branch))
 
 def composer_install():
 	with cd(env.remotepath):
 		run('~/bin/composer install')
-		# run('php70 ~/bin/composer.phar install')
 
-# def clear_cache():
-# 	with cd(env.remotepath):
-# 		run('rm -rf cache/data/*')
+def clear_cache():
+	if ()
+	run ('curl -vs -o /dev/null {0}://{1}/actions/cacheClear/clear?key=fbclear 2>&1'.format(env.remote_protocol, env.hosts[0]))
 
 def pushdb():
 	local("mysqldump -u "+os.environ.get('DB_USER')+" -p'"+os.environ.get('DB_PASS') +
